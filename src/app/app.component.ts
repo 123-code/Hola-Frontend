3ba7545c06720bd2eb6843c9d856d97d3b0186eb
123-code <naranjojose256@gmail.com>
2021-10-05 16:10:56 -0500

    Updated App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hola-Frontend';
+  Patients = [{"Nombre":"Juan","Edad":12,"NTS":3},
+  {"Nombre":"Joaquin","Edad":16,"NTS":6},
+  {"Nombre":"Maria","Edad":20,"NTS":4}];
+
+
+AddPatients():void{
+
+}
+
+DeletePatients():void{
+
+}
+
+UpdatePatients():void{
+
+}
+
+EditPatients():void{
+  
+}
 }
