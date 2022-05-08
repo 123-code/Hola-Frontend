e9f86f62bba57989f84c29dafcc5785ad57843e0
123-code <naranjojose256@gmail.com>
2022-05-07 22:56:59 -0500

    Updated Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,21 +6,42 @@ import {BsInstagram} from 'react-icons/bs';
 import {AiFillGithub} from 'react-icons/ai';
 import {BsTwitter} from 'react-icons/bs';
 import{FaQuestion} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 
 
  const Footer = ()=>{
-     const Icons = [<AiOutlineFacebook/> ,<AiFillGithub/> ,<BsTwitter/>, <GoMail/> ,<BsInstagram onClick={"https://www.instagram.com/holaterapiadelenguaje/?hl=en"}/> ,<FaQuestion/>];
+     const Icons = [<AiOutlineFacebook/> ,"Facebook" ,<AiFillGithub/> ,"Github",<BsTwitter/>, "Twitter",<GoMail/>,"E-mail" ,<BsInstagram onClick={"https://www.instagram.com/holaterapiadelenguaje/?hl=en"}/>,"Instagram" ,<FaQuestion/> ,"Foro"];
+     //const names = ["Facebook","Github","Twitter","Mail","Instagram","Ayuda"];
      return(
         <>
         <div className = "bg-gray-100">
         <img className="object-scale-left h-12 w-12" src={Logo} alt="logo"/>
+        
         {Icons.map((Icon,i)=>{
             return(
            <p key={i}>{Icon}</p>)
         }) }
+      
+        
         </div>
          </>   
      )
  }
  export default Footer;
+//<div className="flex flex-col justify-center items-center">
+const Links = ()=>{
+    const names = ["Facebook","Github","Twitter","Mail","Instagram","Ayuda"];
+    const url = ["https://www.facebook.com/holaterapiadelenguaje/?hl=en",""];
+    return(
+        <>
+        {names.map((name,index,url)=>{
+            return(
+                <>
+                <Link to = {url} key={index}>{name}</Link>
+                </>
+            )
+        })}
+        </>
+    )
+}
--- src/Components/Frase.jsx
+++ src/Components/Frase.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import TxtCards from './TxtCards';
 
 const Frase = ()=>{
     return(
 <>
        <div className="min-h-screen bg-gradient-to-b from-gray-100 to-gray-300 text-center ">
           
-           <h1 className="font-mono text-6xl subpixel-antialiased text-lime-500"> Cambiamos Vidas!</h1>
+           <h1 className="font-mono text-6xl subpixel-antialiased text-lime-500"> Recibir Terapia de lenguaje oportunamente puede marcar la diferencia</h1>
+              
        </div>
 </>
     )}
