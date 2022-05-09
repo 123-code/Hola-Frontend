acf551d98ef198ccf36cb4c1fd3027fc43768379
123-code <naranjojose256@gmail.com>
2022-05-09 09:13:24 -0500

    Updated File

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -29,6 +29,7 @@ import {Link} from 'react-router-dom';
      )
  }
  export default Footer;
+ 
 //<div className="flex flex-col justify-center items-center">
 const Links = ()=>{
     const names = ["Facebook","Github","Twitter","Mail","Instagram","Ayuda"];
--- src/Components/Frase.jsx
+++ src/Components/Frase.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import TxtCards from './TxtCards';
-import CardText from '../CardText.json';
+import{ Card } from './TxtCards';
 
-const Frase = ()=>{
+const Frase = ()=>{ 
     return(
 <>
        <div className="min-h-screen bg-gradient-to-b from-gray-100 to-gray-300 text-center ">
           
            <h1 className="font-mono text-6xl subpixel-antialiased text-lime-500"> Recibir Terapia de lenguaje oportunamente puede marcar la diferencia</h1>
-           <TxtCards/>     
+         
        </div>
 </>
     )}
-
+ 
     export default Frase;
