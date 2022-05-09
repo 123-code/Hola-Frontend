ed10b810d10f18d0aacd15139e88c8cc7e675054
123-code <naranjojose256@gmail.com>
2022-05-09 09:12:58 -0500

    JSON Mapping

--- src/Components/TxtCards.jsx
+++ src/Components/TxtCards.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 
-const TxtCards=(props)=>{
-   return(
-       <>
-       {props.map((props,index)=>{
-           return(
-               <>
-               <div className="Title">
-                   <h1 key={index}>{props.titles}</h1>
-               </div>
+const TxtCards=()=>{
 
-               <div className="Body">
-                   <h2 key = {index}>{props.contents}</h2>
-               </div>
+const CardJson = {
+    "titles":{
+        "Title1":"Pregunta en el Foro",
+        "Title2":"Conoce a Nuestro Equipo ",
+        "Title3":"Ponte en contacto con Nosotros",
+        "Title4":"Consulta por nuestro material de venta"
+        },
+        "contents":{
+            "text1": "Responde a tus dudas relacionadas a la terapia de lenguaje",
+            "text2": "Conoce a nuestro equipo de profesionales",
+            "text3": "Contactanos y Agenda una evaluacion!",
+            "text4": "Encuentra el material que necesites"
+        }
 
-               </>
-           )
-       })
-       
-       }
-       
-       </>
-   ) 
+}
+return(
+    <>
+    <header>
+    <div className = "header">
+ {Object.keys(CardJson.titles).map((title,index)=>{return(<><h2 key={index}>{title[index]}</h2></>)})}
+     </div>
+    </header>
+
+    <body>
+    <div className = "Body">
+ {Object.keys(CardJson.contents).map((content,index)=>{return(<><p key={index}>{content[index]}</p></>)})}
+     </div>
+    </body>
+     </>
+)
+
 }
 export default TxtCards;
 
 
+export function Card(){
+    return(
+        <>
+        {TxtCards()}
+        </>
+    )
+}
