c1a0e668c328203a7d4d607d90e54f36a89705b4
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2023-07-26 17:30:54 +0000

    chat

---
+++ src/Pages/Chat.jsx
@@ -0,0 +1,24 @@
+import React,{ useState,useEffect } from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export default function Chat(){
+    return(
+        <>
+        <div>
+        <Typography variant="h2" gutterBottom>
+        Hola Chat
+      </Typography>
+        </div>
+
+
+        <div>
+        <Typography variant="h3" gutterBottom>
+        Hola Chat
+       </Typography>
+        </div>
+
+        
+        </>
+    )
+
+}
